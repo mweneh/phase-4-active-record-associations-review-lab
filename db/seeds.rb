4b30ffcf4aac32b510a906053d679a838f0c816e
David <davidongaroh@gmail.com>
2022-09-28 18:13:47 +0300

    testing with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'seeding in 3,2,1'
+
+50.times do
+    Passenger.create(name: Faker::Name.name)
+end
+
+40.times do
+    Taxi.create(number: rand(1..100))
+end
+
+100.times do
+    Ride.create(
+    amount:rand(15...200)*10,
+    duration:rand(5..90),
+    taxi_id: Taxi.all[rand(Taxi.count)].id,
+    passenger_id: Passenger.all[rand(Passenger.count)].id
+)
+end 
+
+puts "You seeds have germinated!"
