27f75ca960c2cf76f4c4218127cfa15524292393
David <davidongaroh@gmail.com>
2022-09-28 17:47:11 +0300

    solved

--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -1,6 +1,10 @@
 class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
+      t.integer :passenger_id
+      t.integer :taxi_id
+      t.integer :amount
+      t.integer :duration
 
       t.timestamps
     end
